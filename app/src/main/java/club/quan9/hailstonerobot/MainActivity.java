45ccf32cfa2a515fad79367bf5ae89a115a91c55
ws752499660 <ws752499660@foxmail.com>
2018-05-10 12:49:49 +0800

    initial

---
+++ app/src/main/java/club/quan9/hailstonerobot/MainActivity.java
@@ -0,0 +1,38 @@
+package club.quan9.hailstonerobot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.about:
+                Toast.makeText(this,"关于页面正在施工中",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
